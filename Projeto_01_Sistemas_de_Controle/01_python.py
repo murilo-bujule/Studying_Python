76433dc3296efe79605ab8bbcc96e3468e641169
murilo-bujule <114461335+murilo-bujule@users.noreply.github.com>
2023-06-28 00:24:18 +0000

    checkpoint

--- Projeto_01_Sistemas_de_Controle/01_python.py
+++ Projeto_01_Sistemas_de_Controle/01_python.py
@@ -1,4 +1,20 @@
-import math
+import control as con
+import matplotlib.pyplot as plt
+import numpy as np
 
+A = -40
+B = 2.8
+C = 90
 
-print("Hello!")
+num = np.array([1])
+# den = np.array([ 1, (B-A), (A*B*C), -A*C])
+den = np.array([ 1, 2])
+
+sys = con.TransferFunction(num,den)
+time_simulation = np.arange( 0, 15, 0.5, dtype = float)
+
+[xout, yout] = con.step_response(sys,time_simulation)
+
+plt.figure()
+plt.plot(xout, yout)
+plt.show()
