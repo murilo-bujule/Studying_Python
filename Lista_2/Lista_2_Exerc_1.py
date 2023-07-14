cc58d4c891ff437744d6e056e38777c8e5f0feae
Murilo-bujule <murilo_lemos@discente.ufg.br>
2023-07-14 16:29:31 -0300

    Lista 2 completa

---
+++ Lista_2/Lista_2_Exerc_1.py
@@ -0,0 +1,8 @@
+seq1 = input("Digite a primeira sequência: ")
+seq2 = input("Digite a segunda sequência: ")
+
+if seq1 == seq2:
+    print("Sequências iguais")
+else:
+    print("Sequência diferentes")
+    
